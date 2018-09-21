000e9fac18f2eed936d5e71c3a1a133c760266b3
Lars Ruthotto <lruthotto@emory.edu>
2018-09-21 17:51:55 +0200

    added startup function

---
+++ startupNumDLToolbox.m
@@ -0,0 +1,5 @@
+
+addpath(genpath(pwd))
+
+msg = fprintf('%s\n', '','Welcome to the MATLAB codes for the course Numerical Methods for Deep Learning',...    
+        '','For details see https://github.com/IPAIopen/NumDL-MATLAB','');
